d435b379bc6a860796e4da0523bbc052c550b823
『 Pavel 』 <gaagpavel01@gmail.com>
2020-05-29 14:59:04 +0700

    Добавил алгоритмы фильтров, но пока ничего не привязывал

--- PhotoEditorApp/TableViewController.swift
+++ PhotoEditorApp/TableViewController.swift
@@ -55,6 +55,63 @@ class TableViewController: UITableViewController {
         }
         tableView.deselectRow(at: indexPath, animated: true)
 }
+    
+    func toPixel(image: UIImage) -> UnsafeMutableBufferPointer<UInt32> {
+        let cgImage = image.cgImage
+        let imageData = cgImage?.dataProvider?.data as Data?
+        let size = Int(image.size.width) * Int(image.size.height)
+        let buffer = UnsafeMutableBufferPointer<UInt32>.allocate(capacity: size)
+        imageData?.copyBytes(to: buffer)
+        return buffer
+    }
+
+    func toImage(data: UnsafeMutableBufferPointer<UInt32>, width: Int, height: Int) -> UIImage {
+        let ctx = CGContext(data: data.baseAddress, width: width, height: height, bitsPerComponent: 8, bytesPerRow: width * 4, space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue)!
+        return UIImage(cgImage: ctx.makeImage()!)
+    }
+    
+    func filter(image: UIImage) -> UIImage {
+        let mode = "monochrome"
+        
+        let width = Int(image.size.width)
+        let height = Int(image.size.height)
+        let pixelArray = toPixel(image: image)
+        
+        for y in 0 ..< width {
+            for x in 0 ..< height {
+                let pixelInfo: Int = ((y * width) + x)
+                let hexNumber: UInt32 = pixelArray[pixelInfo]
+                
+                var r = CGFloat((hexNumber & 0xff000000) >> 16) / 255
+                var g = CGFloat((hexNumber & 0x00ff0000) >> 8) / 255
+                var b = CGFloat(hexNumber & 0x0000ff00) / 255
+                
+                if mode == "monochrome" {
+                    let v = 0.3 * r + 0.59 * g + 0.11 * b
+                    let newHexCode = UInt32(v * 255.0) << 16 | UInt32(v * 255.0) << 8 | UInt32(v * 255.0)
+                    pixelArray[pixelInfo] = newHexCode
+                }
+                
+                if mode == "sepia" {
+                    let tr = 0.393 * r + 0.769 * g + 0.189 * b
+                    let tg = 0.349 * r + 0.686 * g + 0.168 * b
+                    let tb = 0.272 * r + 0.534 * g + 0.131 * b
+                    var new_r, new_g, new_b : CGFloat
+                    
+                    let newHexCode = UInt32(tr * 255.0) << 16 | UInt32(tg * 255.0) << 8 | UInt32(tb * 255.0)
+                    pixelArray[pixelInfo] = newHexCode
+                }
+                
+                if mode == "negative" {
+                    let newHexCode = UInt32((255.0 - r) * 255.0) << 16 | UInt32((255.0 - g) * 255.0) << 8 | UInt32((255.0 - b) * 255.0)
+                    pixelArray[pixelInfo] = newHexCode
+                }
+            }
+        }
+        
+        let newImage: UIImage = toImage(data: pixelArray, width: width, height: height)
+        return newImage
+    }
 
     /*
     // Override to support conditional editing of the table view.
